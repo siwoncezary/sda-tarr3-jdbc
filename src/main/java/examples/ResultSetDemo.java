821bda7183905583ddd0cbef642bab13f896401c
cs <siwon.cezary@gmail.com>
2020-07-18 12:56:07 +0200

    result set

---
+++ src/main/java/examples/ResultSetDemo.java
@@ -0,0 +1,25 @@
+package examples;
+
+import connection.Connections;
+
+import java.sql.*;
+
+public class ResultSetDemo {
+    public static void main(String[] args) throws SQLException {
+        Connection con = Connections.EMPLOYEES.getConnection();
+        //możliwość skrolowania w obie strony w trybie tylko do odczytu
+        Statement statement = con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+        ResultSet resultSet = statement.executeQuery("select * from employees limit 100");
+        while(resultSet.next()){
+            String firstName = resultSet.getString("first_name");
+            String lastName = resultSet.getString("last_name");
+            Date birthDate = resultSet.getDate("birth_date");
+            String gender = resultSet.getString("gender");
+            System.out.println(firstName +" "+ lastName +" " + birthDate +" " + gender);
+        }
+        while(resultSet.previous()){
+            String gender = resultSet.getString("gender");
+            System.out.println(gender);
+        }
+    }
+}
---
+++ src/main/java/examples/ResultSetNullDemo.java
@@ -0,0 +1,22 @@
+package examples;
+import connection.Connections;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class ResultSetNullDemo {
+    public static void main(String[] args) throws SQLException {
+        Connection con = Connections.MARIADB.getConnection();
+        ResultSet set = con.createStatement().executeQuery("select * from sellers");
+        while(set.next()){
+            String name = set.getString("last_name");
+            int salary = set.getInt("salary");
+            if (set.wasNull()) {
+                System.out.println(name + " salary is null");
+            } else {
+                System.out.println(name + " " + salary);
+            }
+        }
+    }
+}
